6a08e6fa80ea0a000477f5f28eec4db741cf8e3d
julihocc <julihocc@yahoo.com>
2024-11-06 18:10:15 -0600

    Refactor AwsCognitoService to use AWS SDK v3 for Cognito operations

--- src/auth/aws-cognito.service.ts
+++ src/auth/aws-cognito.service.ts
@@ -1,82 +1,78 @@
 import { Injectable } from '@nestjs/common';
 import {
-  AuthenticationDetails,
-  CognitoUser,
-  CognitoUserAttribute,
-  CognitoUserPool,
-} from 'amazon-cognito-identity-js';
+  CognitoIdentityProviderClient,
+  SignUpCommand,
+  InitiateAuthCommand,
+  ChangePasswordCommand,
+  ForgotPasswordCommand,
+  ConfirmForgotPasswordCommand,
+  AuthFlowType,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { AuthChangePasswordUserDto } from './dtos/auth-change-password-user.dto';
 import { AuthLoginUserDto } from './dtos/auth-login-user.dto';
 import { AuthRegisterUserDto } from './dtos/auth-register-user.dto';
-
 import { AuthConfirmPasswordUserDto } from './dtos/auth-confirm-password-user.dto';
 import { AuthForgotPasswordUserDto } from './dtos/auth-forgot-password-user.dto';
 
 @Injectable()
 export class AwsCognitoService {
-  private userPool: CognitoUserPool;
+  private cognitoClient: CognitoIdentityProviderClient;
+  private userPoolId: string;
+  private clientId: string;
 
   constructor() {
-    this.userPool = new CognitoUserPool({
-      UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
-      ClientId: process.env.AWS_COGNITO_CLIENT_ID,
-    });
+    this.cognitoClient = new CognitoIdentityProviderClient({});
+    this.userPoolId = process.env.AWS_COGNITO_USER_POOL_ID;
+    this.clientId = process.env.AWS_COGNITO_CLIENT_ID;
   }
 
   async registerUser(authRegisterUserDto: AuthRegisterUserDto) {
     const { name, email, password } = authRegisterUserDto;
     console.log(name, email, password);
 
-    return new Promise((resolve, reject) => {
-      this.userPool.signUp(
-        email,
-        password,
-        [
-          new CognitoUserAttribute({
-            Name: 'name',
-            Value: name,
-          }),
-        ],
-        null,
-        (err, result) => {
-          if (!result) {
-            reject(err);
-          } else {
-            console.log('result.user', result.user);
-            resolve(result.user);
-          }
+    const command = new SignUpCommand({
+      ClientId: this.clientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: 'name',
+          Value: name,
         },
-      );
+      ],
     });
+
+    try {
+      const result = await this.cognitoClient.send(command);
+      console.log('result.user', result);
+      return result;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async authenticateUser(authLoginUserDto: AuthLoginUserDto) {
     const { email, password } = authLoginUserDto;
-    const userData = {
-      Username: email,
-      Pool: this.userPool,
-    };
 
-    const authenticationDetails = new AuthenticationDetails({
-      Username: email,
-      Password: password,
+    const command = new InitiateAuthCommand({
+      AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+      ClientId: this.clientId,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
     });
 
-    const userCognito = new CognitoUser(userData);
-
-    return new Promise((resolve, reject) => {
-      userCognito.authenticateUser(authenticationDetails, {
-        onSuccess: (result) => {
-          resolve({
-            accessToken: result.getAccessToken().getJwtToken(),
-            refreshToken: result.getRefreshToken().getToken(),
-          });
-        },
-        onFailure: (err) => {
-          reject(err);
-        },
-      });
-    });
+    try {
+      const result = await this.cognitoClient.send(command);
+      return {
+        accessToken: result.AuthenticationResult.AccessToken,
+        refreshToken: result.AuthenticationResult.RefreshToken,
+      };
+    } catch (err) {
+      console.error(`Error: ${err}`);
+      throw err;
+    }
   }
 
   async changeUserPassword(
@@ -84,38 +80,30 @@ export class AwsCognitoService {
   ) {
     const { email, currentPassword, newPassword } = authChangePasswordUserDto;
 
-    const userData = {
-      Username: email,
-      Pool: this.userPool,
-    };
-
-    const authenticationDetails = new AuthenticationDetails({
-      Username: email,
-      Password: currentPassword,
+    const authCommand = new InitiateAuthCommand({
+      AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+      ClientId: this.clientId,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: currentPassword,
+      },
     });
 
-    const userCognito = new CognitoUser(userData);
+    try {
+      const authResult = await this.cognitoClient.send(authCommand);
+      const accessToken = authResult.AuthenticationResult.AccessToken;
 
-    return new Promise((resolve, reject) => {
-      userCognito.authenticateUser(authenticationDetails, {
-        onSuccess: () => {
-          userCognito.changePassword(
-            currentPassword,
-            newPassword,
-            (err, result) => {
-              if (err) {
-                reject(err);
-                return;
-              }
-              resolve(result);
-            },
-          );
-        },
-        onFailure: (err) => {
-          reject(err);
-        },
+      const changePasswordCommand = new ChangePasswordCommand({
+        AccessToken: accessToken,
+        PreviousPassword: currentPassword,
+        ProposedPassword: newPassword,
       });
-    });
+
+      const result = await this.cognitoClient.send(changePasswordCommand);
+      return result;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async forgotUserPassword(
@@ -123,23 +111,17 @@ export class AwsCognitoService {
   ) {
     const { email } = authForgotPasswordUserDto;
 
-    const userData = {
+    const command = new ForgotPasswordCommand({
+      ClientId: this.clientId,
       Username: email,
-      Pool: this.userPool,
-    };
-
-    const userCognito = new CognitoUser(userData);
-
-    return new Promise((resolve, reject) => {
-      userCognito.forgotPassword({
-        onSuccess: (result) => {
-          resolve(result);
-        },
-        onFailure: (err) => {
-          reject(err);
-        },
-      });
     });
+
+    try {
+      const result = await this.cognitoClient.send(command);
+      return result;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async confirmUserPassword(
@@ -147,22 +129,18 @@ export class AwsCognitoService {
   ) {
     const { email, confirmationCode, newPassword } = authConfirmPasswordUserDto;
 
-    const userData = {
+    const command = new ConfirmForgotPasswordCommand({
+      ClientId: this.clientId,
       Username: email,
-      Pool: this.userPool,
-    };
-
-    const userCognito = new CognitoUser(userData);
-
-    return new Promise((resolve, reject) => {
-      userCognito.confirmPassword(confirmationCode, newPassword, {
-        onSuccess: () => {
-          resolve({ status: 'success' });
-        },
-        onFailure: (err) => {
-          reject(err);
-        },
-      });
+      ConfirmationCode: confirmationCode,
+      Password: newPassword,
     });
+
+    try {
+      await this.cognitoClient.send(command);
+      return { status: 'success' };
+    } catch (err) {
+      throw err;
+    }
   }
 }
